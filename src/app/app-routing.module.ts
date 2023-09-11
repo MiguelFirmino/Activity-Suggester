1a21278f08e48ddab7a1c45d10df84987cf235a5
MiguelFirmino <miguel.firmino>
2023-09-11 17:02:05 -0300

    Created Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActivitiesComponent } from './activities/activities.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ActivitiesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivitiesComponent } from './activities/activities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivitiesComponent
   ],
   imports: [
     BrowserModule,
